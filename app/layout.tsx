3a3b0e9c539c14ec1aa744cf24fc32ae23e7ef09
petefranka <peteratkinson1994@gmail.com>
2026-05-24 00:24:35 +0100

    move inside body

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,15 +65,16 @@ export default function RootLayout({
         {children}
         <VercelAnalytics />
         <VercelSpeedInsights />
-      </body>
-      {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
-        <Script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-          strategy="afterInteractive"
-        />
+           {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="afterInteractive"
+          />
       )}
+      </body>
+   
     </html>
   );
 }
