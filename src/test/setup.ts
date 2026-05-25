e2425ffb39168119424108ba0793ad7f4be69383
petefranka <peteratkinson1994@gmail.com>
2026-05-25 21:53:23 +0100

    feat:a118y tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,8 @@
 import '@testing-library/jest-dom';
-import { vi } from 'vitest';
+import { vi, expect } from 'vitest';
+import { toHaveNoViolations } from 'jest-axe';
+
+expect.extend(toHaveNoViolations);
 
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
